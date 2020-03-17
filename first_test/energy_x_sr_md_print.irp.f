a83e01501e33008506e73e278dbe62a7159b4edc
dtraore97 <diatatraore@yahoo.com>
2020-03-17 17:49:08 +0100

    Veuillez saisir le message de validation pour vos modifications. Les lignes  commençant par '' seront ignorées, et un message vide abandonne la validation.
     Sur la branche master
     Votre branche est à jour avec 'origin/master'.
    
     Modifications qui seront validées :
    	supprimé :        first_test/copy_program_sr_pbe_md.irp.f
    	supprimé :        first_test/energy_x_sr_md_print.irp.f
    	supprimé :        first_test/sr_pbe_md.irp.f
    	supprimé :        first_test/sr_pbe_md_version_2.irp.f
    	nouveau fichier : sr_md_energies/.gitignore
    	nouveau fichier : sr_md_energies/NEED
    	nouveau fichier : sr_md_energies/README.rst
    	nouveau fichier : sr_md_energies/copy_program_sr_pbe_md_2
    	renommé :         first_test/copy_program_sr_pbe_md_2.irp.f -> sr_md_energies/copy_program_sr_pbe_md_2.irp.f
    	nouveau fichier : sr_md_energies/dft_mu_of_r
    	nouveau fichier : sr_md_energies/dft_one_e
    	nouveau fichier : sr_md_energies/energy_x_sr_md_print
    	nouveau fichier : sr_md_energies/energy_x_sr_md_print.irp.f
    	nouveau fichier : sr_md_energies/functionals
    	nouveau fichier : sr_md_energies/hollet_pegoretti_func
    	nouveau fichier : sr_md_energies/ijkl_ints_in_r3
    	nouveau fichier : sr_md_energies/mu_of_r
    	nouveau fichier : sr_md_energies/n_or_ao_prod_derivative
    	nouveau fichier : sr_md_energies/proj_operators
    	nouveau fichier : sr_md_energies/rsdft_cipsi
    	nouveau fichier : sr_md_energies/rsdft_ecmd
    	nouveau fichier : sr_md_energies/sr_md_energies
    	nouveau fichier : sr_md_energies/sr_pbe_md_v2.irp.f
    	nouveau fichier : sr_md_energies/two_bod_energy_spin
    	nouveau fichier : sr_md_energies/two_body_dm
    	nouveau fichier : sr_md_energies/two_body_rdm
    
     Fichiers non suivis:
    	first_test/copy_program_sr_pbe_md
    	first_test/copy_program_sr_pbe_md_2
    	first_test/dft_mu_of_r
    	first_test/dft_one_e
    	first_test/effective_potential_test
    	first_test/energy_x_sr_md_print
    	first_test/first_test
    	first_test/first_test2
    	first_test/functionals
    	first_test/hollet_pegoretti_func
    	first_test/ijkl_ints_in_r3
    	first_test/mu_of_r
    	first_test/n_or_ao_prod_derivative
    	first_test/new_schro
    	first_test/plot_2dm
    	first_test/potential
    	first_test/proj_operators
    	first_test/rsdft_cipsi
    	first_test/rsdft_ecmd
    	first_test/two_bod_energy_spin
    	first_test/two_body_dm
    	first_test/two_body_rdm

--- first_test/energy_x_sr_md_print.irp.f
+++
@@ -1,5 +0,0 @@
-program energy_md
-print*,'Energy_x_sr_pbe_md= ', energy_x_sr_pbe_md(1),',  Energy_x_sr_pbe_md_version2=', energy_x_sr_pbe_2(1), ' , Energy_x_sr_pbe= ', energy_x_sr_pbe
-print*,'Energy_c_sr_pbe_md= ', energy_c_sr_pbe_md(1),',  Energy_c_sr_pbe_md_version2=', energy_c_sr_pbe_2(1), ' , Energy_c_sr_pbe= ', energy_c_sr_pbe
-end program
-
