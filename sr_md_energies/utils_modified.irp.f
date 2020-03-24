0a8b6678abddc3547ba652f0c6e696193b90d34c
dtraore97 <diatatraore@yahoo.com>
2020-03-24 17:35:21 +0100

    Veuillez saisir le message de validation pour vos modifications. Les lignes  commençant par '' seront ignorées, et un message vide abandonne la validation.
     Sur la branche master
     Votre branche est à jour avec 'origin/master'.
    
     Modifications qui seront validées :
    	modifié :         copy_program_sr_pbe_md_2
    	modifié :         utils_modified.irp.f
    
     Fichiers non suivis:
    	../first_test/dft_mu_of_r
    	../first_test/dft_one_e
    	../first_test/effective_potential_test
    	../first_test/first_test
    	../first_test/first_test2
    	../first_test/functionals
    	../first_test/hollet_pegoretti_func
    	../first_test/ijkl_ints_in_r3
    	../first_test/mu_of_r
    	../first_test/n_or_ao_prod_derivative
    	../first_test/new_schro
    	../first_test/plot_2dm
    	../first_test/potential
    	../first_test/proj_operators
    	../first_test/rsdft_cipsi
    	../first_test/rsdft_ecmd
    	../first_test/two_bod_energy_spin
    	../first_test/two_body_dm
    	../first_test/two_body_rdm
    	energy_xc_sr_md_print

--- sr_md_energies/utils_modified.irp.f
+++ sr_md_energies/utils_modified.irp.f
@@ -1,17 +1,20 @@
 
 subroutine GGA_sr_type_functionals_mu(mu,r,rho_a,rho_b,grad_rho_a_2,grad_rho_b_2,grad_rho_a_b, &
                                 ex,vx_rho_a,vx_rho_b,vx_grad_rho_a_2,vx_grad_rho_b_2,vx_grad_rho_a_b, &
-                                ec,vc_rho_a,vc_rho_b,vc_grad_rho_a_2,vc_grad_rho_b_2,vc_grad_rho_a_b )
+                                ec,vc_rho_a,vc_rho_b,vc_grad_rho_a_2,vc_grad_rho_b_2,vc_grad_rho_a_b ) 
+ 
  implicit none
  BEGIN_DOC
  ! routine that helps in building the x/c potentials on the AO basis for a GGA functional with a short-range interaction
  END_DOC
  double precision, intent(in)  :: r(3),rho_a(N_states),rho_b(N_states),grad_rho_a_2(N_states),grad_rho_b_2(N_states),grad_rho_a_b(N_states),mu
- double precision, intent(out) :: ex(N_states),vx_rho_a(N_states),vx_rho_b(N_states),vx_grad_rho_a_2(N_states),vx_grad_rho_b_2(N_states),vx_grad_rho_a_b(N_states)
- double precision, intent(out) :: ec(N_states),vc_rho_a(N_states),vc_rho_b(N_states),vc_grad_rho_a_2(N_states),vc_grad_rho_b_2(N_states),vc_grad_rho_a_b(N_states)
+ double precision, intent(out) :: ex(N_states),vx_rho_pbe_a(N_states),vx_rho_pbe_b(N_states),vx_grad_rho_pbe_a_2(N_states),vx_grad_rho_pbe_b_2(N_states),vx_grad_rho_pbe_a_b(N_states),vx_rho_sr_pbe_md_a(N_states),vx_rho_sr_pbe_md_b(N_states),vx_grad_rho_sr_pbe_md_a_2(N_states),vx_grad_rho_sr_pbe_md_b_2(N_states),vx_grad_rho_sr_pbe_md_a_b(N_states)
+ double precision, intent(out) :: ec(N_states),vc_rho_pbe_a(N_states),vc_rho_pbe_b(N_states),vc_grad_rho_pbe_a_2(N_states),vc_grad_rho_pbe_b_2(N_states),vc_grad_rho_pbe_a_b(N_states), vc_rho_sr_pbe_md_a(N_states),vc_rho_sr_pbe_md_b(N_states),vc_grad_rho_sr_pbe_md_a_2(N_states),vc_grad_rho_sr_pbe_md_b_2(N_states),vc_grad_rho_sr_pbe_md_a_b(N_states)
  integer          :: istate
  double precision :: r2(3),dr2(3), local_potential,r12,dx2
+ 
  do istate = 1, N_states
+ !------------------Usual PBE--------------------
   call ex_pbe_sr(mu,rho_a(istate),rho_b(istate),grad_rho_a_2(istate),grad_rho_b_2(istate),grad_rho_a_b(istate),ex(istate),vx_rho_a(istate),vx_rho_b(istate),vx_grad_rho_a_2(istate),vx_grad_rho_b_2(istate),vx_grad_rho_a_b(istate))
 
   double precision :: rhoc,rhoo,sigmacc,sigmaco,sigmaoo,vrhoc,vrhoo,vsigmacc,vsigmaco,vsigmaoo
@@ -21,8 +24,27 @@ subroutine GGA_sr_type_functionals_mu(mu,r,rho_a,rho_b,grad_rho_a_2,grad_rho_b_2
 
    call ec_pbe_sr(mu,rhoc,rhoo,sigmacc,sigmaco,sigmaoo,ec(istate),vrhoc,vrhoo,vsigmacc,vsigmaco,vsigmaoo)
 
-   call v_rho_oc_to_v_rho_ab(vrhoo,vrhoc,vc_rho_a(istate),vc_rho_b(istate))
+ !-------------------Multideterminant sr-PBE-----------------
+ pi = dacos(-1.d0)
+ beta_rho_delta_rho_m = 3.d0 /(2.d0*dsqrt(pi)*(1.d0 - dsqrt(2.d0)))
+ rho = rho_a(istate) + rho_b(istate)
+! g0 = g0_UEG_inf(rho_a(istate), rho_b(istate))
+! n2_UEG = (rho**2)*g0
+
+ ! -------------------Exchange---------------------
+ vx_rho_sr_pbe_md_a(istate)
+ vx_rho_sr_pbe_md_b(istate)
+ vx_grad_rho_sr_pbe_md_a_2(istate)
+ vx_grad_rho_sr_pbe_md_b_2(istate)
+ vx_grad_rho_sr_pbe_md_a_b(istate)
+ ! ---------------- Correlation -------------------
+ vc_rho_sr_pbe_md_a(istate)
+ vc_rho_sr_pbe_md_b(istate)
+ vc_grad_rho_sr_pbe_md_a_2(istate)
+ vc_grad_rho_sr_pbe_md_b_2(istate)
+ vc_grad_rho_sr_pbe_md_a_b(istate)
+ !--------------------------Convert--------------------------
+   call v_rho_oc_to_v_rho_ab(vrhoo,vrhoc,vc_rho_a(istate),vc_rho_b(istate)) ! conversion closed/open -> alpha/beta
    call v_grad_rho_oc_to_v_grad_rho_ab(vsigmaoo,vsigmacc,vsigmaco,vc_grad_rho_a_2(istate),vc_grad_rho_b_2(istate),vc_grad_rho_a_b(istate))
  enddo
 end
-
